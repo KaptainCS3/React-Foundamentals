da16e85c5fc536ab0647bd816354831c5730da57
KaptainCS3 <mbiapplegryn@gmail.com>
2022-08-13 13:50:03 +0100

    added fontawesome icons

--- dropdown/src/components/Footer.js
+++ dropdown/src/components/Footer.js
@@ -1,14 +1,23 @@
 import React from 'react'
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {faGithub, faInstagram, faTwitter, faLinkedin} from '@fortawesome/free-brands-svg-icons'
 function Footer() {
   return (
     <footer>
       <nav className="nav-footer">
         <ul className="nav-bar-footer">
-          <li>GitHub</li>
-          <li>Instagram</li>
-          <li>Twitter</li>
-          <li>Linkedin</li>
+          <li>
+            <FontAwesomeIcon icon={faGithub} /> GitHub
+          </li>
+          <li>
+            <FontAwesomeIcon icon={faInstagram} /> Instagram
+          </li>
+          <li>
+            <FontAwesomeIcon icon={faTwitter} /> Twitter
+          </li>
+          <li>
+            <FontAwesomeIcon icon={faLinkedin} /> Linkedin
+          </li>
         </ul>
         <p>
           <b>&#169;</b> 2022. All Right Reserved by (KaptainCS3)
