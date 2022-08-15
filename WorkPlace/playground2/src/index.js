cae530bf26bcaa3f13c6cd08824339a0fcc697b1
KaptainCS3 <mbiapplegryn@gmail.com>
2022-08-15 12:50:56 +0100

    markup and setup done

--- WorkPlace/playground2/src/index.js
+++ WorkPlace/playground2/src/index.js
@@ -1,38 +1,45 @@
 import React from "react";
 import ReactDom from "react-dom";
-
-const Nav = () =>{
-  return (
-    <nav>
-      <p>MYlogo</p>
-      <ul>
-        <li>Pricing</li>
-        <li>About</li>
-        <li>Contact</li>
-      </ul>
-    </nav>
-  );
-}   
-
-const MyPage = () =>{
-  return (
-    <>
-      <ul>
-        List of Cafe stores
-        <li>Local shop</li>
-        <li>SuperMarket</li>
-        <li>I don't know any other option</li>
-      </ul>
-    </>
-  );
-}
+import Nav from './component/Nav'
+import Main from './component/Main'
+import Cards from './component/Cards'
+import './index.css'
+import img from './images/image 12.png'
+import wedding from './images/wedding-photography 1.png'
+import bike from './images/mountain-bike 1.png'
 
 const App = () =>{
-  return(
-    <>
-    <Nav />
-    <MyPage />
-    </>
-  )
+  return (
+    <div className="container">
+      <Nav />
+      <Main />
+      <div className="row">
+        <Cards
+          img = {img}
+          alt="Zaferes USA"
+          rating="5.0"
+          grayTxt="(6). USA"
+          name="Life lessons with Katie Zaferes"
+          des="From $136"
+        />
+        <Cards
+          img={wedding}
+          alt='wedding photography'
+          rating="5.0"
+          grayTxt="(30). USA"
+          name="Learn wedding photography"
+          des="From $125"
+        />
+        <Cards
+          img={bike}
+          alt = 'Mountain bike'
+          rating="4.8"
+          grayTxt="(2). USA"
+          name="Group Mountain Bikin"
+          des="From $50"
+        />
+      </div>
+    </div>
+  );
 }
 ReactDom.render(<App />, document.getElementById("root"));
