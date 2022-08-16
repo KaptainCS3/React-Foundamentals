2434b36b3eb04e325b6a74a87f2ac0bea4ff07ea
KaptainCS3 <mbiapplegryn@gmail.com>
2022-08-16 16:14:37 +0100

    initial commit

--- react-tms/src/index.js
+++ react-tms/src/index.js
@@ -1,38 +1,32 @@
 import React from "react";
 import ReactDom from "react-dom";
-
-const Nav = () =>{
-  return (
-    <nav>
-      <p>MYlogo</p>
-      <ul>
-        <li>Pricing</li>
-        <li>About</li>
-        <li>Contact</li>
-      </ul>
-    </nav>
-  );
-}   
-
-const MyPage = () =>{
-  return (
-    <>
-      <ul>
-        List of Cafe stores
-        <li>Local shop</li>
-        <li>SuperMarket</li>
-        <li>I don't know any other option</li>
-      </ul>
-    </>
-  );
-}
+import Jokes from "./component/Jokes"
 
 const App = () =>{
   return(
     <>
-    <Nav />
-    <MyPage />
+    <Jokes 
+      quotes = "If you want to achieve greatness stop asking for permission. ~Anonymous"
+      punchline = ""
+    />
+    <Jokes 
+      quotes = "To live a creative life, we must lose our fear of being wrong. ~Anonymous"
+      punchline = ""
+    />
+    <Jokes 
+      quotes = "If you are not willing to risk the usual you will have to settle for the ordinary. ~Jim Rohn"
+      punchline = ""
+    />
+    <Jokes 
+      quotes = "Trust because you are willing to accept the risk, not because it's safe or certain. ~Anonymous"
+      punchline = ""
+    />
+    <Jokes 
+    quotes = "Take up one idea. Make that one idea your life - think of it, dream of it, live on that idea. Let the brain, muscles, nerves, every part of your body, be full of that idea, and just leave every other idea alone. This is the way to success. ~Swami Vivekananda"
+      punchline = ""
+    />
     </>
   )
 }
-ReactDom.render(<App />, document.getElementById("root"));
+
+ReactDom.render(<App />, document.getElementById('root'))
