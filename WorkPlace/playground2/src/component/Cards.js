9d946c48072c5a65cd4872402b513ee285ffd1e5
KaptainCS3 <mbiapplegryn@gmail.com>
2022-08-22 13:24:35 +0100

    AirBnB sample done

--- WorkPlace/playground2/src/component/Cards.js
+++ WorkPlace/playground2/src/component/Cards.js
@@ -1,26 +1,39 @@
 import React from "react";
-import star from '../images/Star 1.png'
-const Cards = ({img, alt, rating, grayTxt, title, price}) =>{
-  // console.log(props)
+import star from "../images/Star 1.png";
+const Cards = (props) => {
+  console.log(props);
+  let badgeText;
+  if (props.el.openSpots === 0) {
+    badgeText = "SOLD OUT";
+  } else if (props.el.location === "Online") {
+    badgeText = "ONLINE";
+  }
   return (
     <>
       <div className="cards">
-        <img src={img} alt={alt} className = 'img'/>
+        {badgeText && <div className="badge">{badgeText}</div>}
+        <img
+          src={require(`../images${props.el.coverImg}`)}
+          alt=""
+          className="img"
+        />
         <div className="card-content">
           <span className="rating">
             <span>
               <img src={star} alt="start" />
             </span>
-            <span>{rating}</span>
-            <span className="usa">{grayTxt}</span>
+            <span>{props.el.stats.rating}</span>
+            <span className="usa">
+              ({props.el.stats.reviewCount}){props.el.location}
+            </span>
           </span>
-          <p className="card-profile">{title}</p>
+          <p className="card-profile">{props.el.title}</p>
           <span className="person">
-            <b>{price}</b> / person
+            <b>from ${props.el.price}</b> / person
           </span>
         </div>
       </div>
     </>
   );
-}
-export default Cards
+};
+export default Cards;
