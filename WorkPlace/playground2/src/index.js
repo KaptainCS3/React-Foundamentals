d5bd95fbb338fb41f35886831b2cd4a7424c2851
KaptainCS3 <mbiapplegryn@gmail.com>
2022-08-22 13:24:49 +0100

    AirBnB sample done

--- WorkPlace/playground2/src/index.js
+++ WorkPlace/playground2/src/index.js
@@ -4,9 +4,29 @@ import Nav from './component/Nav'
 import Main from './component/Main'
 import Cards from './component/Cards'
 import './index.css'
-import img from './images/image 12.png'
-import wedding from './images/wedding-photography 1.png'
-import bike from './images/mountain-bike 1.png'
+import data from './data'
+
+  const cardData = data.map(el =>{
+        return (
+          <Cards 
+            keys= {el.id}
+            // assign the hold props to data alternative #1
+            el = {el}
+
+            // assigning props to the data object alternative #2
+            // title= {el.title}
+            // description={el.description}
+            // price={el.price}
+            // coverImg = {el.coverImg}
+            // rating={el.stats.rating}
+            // reviewCount={el.stats.reviewCount}
+            // location= {el.location}
+            
+            //assign props to the data object alternative #3
+            // {...el}
+          />
+        )
+  })
 
 const App = () =>{
   return (
@@ -14,30 +34,7 @@ const App = () =>{
       <Nav />
       <Main />
       <div className="row">
-        <Cards
-          img = {img}
-          alt="Zaferes USA"
-          rating="5.0"
-          grayTxt="(6). USA"
-          title="Life lessons with Katie Zaferes"
-          price="From $136"
-        />
-        <Cards
-          img={wedding}
-          alt='wedding photography'
-          rating="5.0"
-          grayTxt="(30). USA"
-          title="Learn wedding photography"
-          price="From $125"
-        />
-        <Cards
-          img={bike}
-          alt = 'Mountain bike'
-          rating="4.8"
-          grayTxt="(2). USA"
-          title="Group Mountain Bikin"
-          price="From $50"
-        />
+        {cardData}
       </div>
     </div>
   );
