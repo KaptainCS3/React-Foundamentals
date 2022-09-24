5a307e288143148f0e36830f3bc96c86abe1bb1f
KaptainCS3 <mbiapplegryn@gmail.com>
2022-09-24 11:30:25 +0100

    footer impimentation as bonus:

--- dropdown/src/components/Footer.js
+++ dropdown/src/components/Footer.js
@@ -1,30 +1,49 @@
-import React from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import {faGithub, faInstagram, faTwitter, faLinkedin} from '@fortawesome/free-brands-svg-icons'
-function Footer() {
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faGithub,
+  faInstagram,
+  faTwitter,
+  faLinkedin,
+} from "@fortawesome/free-brands-svg-icons";
+function Footer({ darkMode }) {
+  const links = {
+    github: "https://github.com/KaptainCS3",
+    instagram: "https://www.instagram.com/leonard_appelgryn_",
+    twitter: "https://twitter.com/KaptainCS3",
+    linkedin: "",
+  };
   return (
-    <footer>
-      <nav className="nav-footer">
+    <footer className={darkMode ? "dark" : ""}>
+      <div className="nav-footer">
         <ul className="nav-bar-footer">
           <li>
-            <FontAwesomeIcon icon={faGithub} /> GitHub
+            <a href={links.github} className={darkMode ? "dark" : ""}>
+              <FontAwesomeIcon icon={faGithub} /> GitHub
+            </a>
           </li>
           <li>
-            <FontAwesomeIcon icon={faInstagram} /> Instagram
+            <a href={links.instagram} className={darkMode ? "dark" : ""}>
+              <FontAwesomeIcon icon={faInstagram} /> Instagram
+            </a>
           </li>
           <li>
-            <FontAwesomeIcon icon={faTwitter} /> Twitter
+            <a href={links.twitter} className={darkMode ? "dark" : ""}>
+              <FontAwesomeIcon icon={faTwitter} /> Twitter
+            </a>
           </li>
           <li>
-            <FontAwesomeIcon icon={faLinkedin} /> Linkedin
+            <a href={links.linkedin} className={darkMode ? "dark" : ""}>
+              <FontAwesomeIcon icon={faLinkedin} /> Linkedin
+            </a>
           </li>
         </ul>
-        <p>
+        <p className={darkMode ? "dark" : ""}>
           <b>&#169;</b> 2022. All Right Reserved by (KaptainCS3)
         </p>
-      </nav>
+      </div>
     </footer>
   );
 }
 
-export default Footer
+export default Footer;
