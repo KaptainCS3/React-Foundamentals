1e3632cadade1157cc1bcd73a7ee4954b2f82370
KaptainCS3 <mbiapplegryn@gmail.com>
2022-09-24 11:26:04 +0100

    Render App component

--- dropdown/src/index.js
+++ dropdown/src/index.js
@@ -1,17 +1,5 @@
 import React from "react";
 import ReactDom from "react-dom";
+import App from "./App";
 import "./style.css";
-import Nav from "./components/Nav";
-import Main from "./components/Main";
-import Footer from "./components/Footer";
-
-function App() {
-  return (
-    <div className="container">
-      <Nav />
-      <Main />
-      <Footer />
-    </div>
-  );
-}
 ReactDom.render(<App />, document.getElementById("root"));
